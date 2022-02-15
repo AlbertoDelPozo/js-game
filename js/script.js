efd5ac3335f20835db797d76a764c64e3d40aef1
AlbertoDelPozo <72515390+AlbertoDelPozo@users.noreply.github.com>
2022-02-15 13:03:14 +0100

    Juego terminado 100%

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 class Cambio extends HTMLElement {
     constructor () {
         super ();
@@ -15,3 +17,49 @@ class Cambio extends HTMLElement {
     }
 }
 customElements.define('wc-cambio', Cambio);
+
+const templateInstuctions = document.createElement('template');
+
+templateInstuctions.innerHTML = `
+    <style>
+        h1 {
+            font-size: 3em;
+        }
+        template-instructions {
+            font-family: 'Press Start 2P', cursive;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+        div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+    </style>
+    <template-instructions>
+        <div>
+            <h1>CAN GAME</h1>
+        </div>
+        <br>
+        <div class="d-flex justify-content-center align-items-center flex-column">
+            <p>1. Shoot all the guns to win before the time ends</p>
+            <p>2. You can stop the game but you can't shoot at the cans when they don't move</p>
+            <p>3. You can continue the game clicking RESTART</p>
+            <p>4. To reload the game press RESET</p>
+            <p>5. HAVE FUN :)</p>
+        </div>
+    </template-instructions>
+`;
+
+customElements.define('template-instructions',
+    class extends HTMLElement {
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow( { mode: 'open' });
+            this._shadowRoot.appendChild(templateInstuctions.content.cloneNode(true));
+        }
+    }
+)
